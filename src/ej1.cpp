b2e06ad3e9e408362e6bc614180eb6553e894910
Agustin Borgna <hello@aborgna.com.ar>
2016-08-21 13:03:35 -0300

    estaba el revez el orden de entrada de N y P
    * pasar el código por un formatter

--- src/ej1.cpp
+++ src/ej1.cpp
@@ -1,55 +1,56 @@
-#include <iostream>
-#include <cstdio>
 #include <algorithm>
-#include <vector>
-#include <set>
+#include <cstdio>
+#include <iostream>
 #include <map>
+#include <set>
+#include <vector>
 
-#define forn(i,n) for(int i =0; i < (int)(n); i++)
+#define forn(i, n) for (int i = 0; i < (int)(n); i++)
 
 using namespace std;
 const int maxn = 40;
-int N,A,B;
-long long P [maxn],Q[maxn],T,resp;
-int main(){
-	cin>>N>>T;
-	A = N/2;
-	B = N/2 + N%2;
-	forn(i,A)
-		cin>>P[i];
-	forn(i,B)
-		cin>>Q[i];
-	set<long long> resultados;
-	forn(i,1<<A){
-		long long suma=0;
-		forn(j,A){
-			if((1<<j & i) == (1<<j))
-				suma+=P[j];
-		}
-		if(suma<=T)
-			resultados.insert(suma);
-	}
-	forn(i,1<<B){
-		long long busco=0;
-		forn(j,B){
-			if((1<<j & i) == (1<<j))
-				busco+=Q[j];
-		}
+int N, A, B;
+long long P[maxn], Q[maxn], T, resp;
 
-		if(busco>T) continue;
-		auto it = resultados.lower_bound(T - busco);
-		if(it == resultados.end())
-			it--;
-		if(it==resultados.begin())
-			resp= max(resp,busco);
-		else{
-			if(*it > T - busco) it--;
-			if(it==resultados.begin())
-				resp= max(resp,busco);
-			else
-				resp = max(resp,*it+busco);
-		}
-	}
-	cout<<resp<<endl;
-	return 0;
+int main() {
+    cin >> T >> N;
+    A = N / 2;
+    B = N / 2 + N % 2;
+    forn(i, A) cin >> P[i];
+    forn(i, B) cin >> Q[i];
+    set<long long> resultados;
+    forn(i, 1 << A) {
+        long long suma = 0;
+        forn(j, A) {
+            if ((1 << j & i) == (1 << j))
+                suma += P[j];
+        }
+        if (suma <= T)
+            resultados.insert(suma);
+    }
+    forn(i, 1 << B) {
+        long long busco = 0;
+        forn(j, B) {
+            if ((1 << j & i) == (1 << j))
+                busco += Q[j];
+        }
+
+        if (busco > T)
+            continue;
+        auto it = resultados.lower_bound(T - busco);
+        if (it == resultados.end())
+            it--;
+        if (it == resultados.begin())
+            resp = max(resp, busco);
+        else {
+            if (*it > T - busco)
+                it--;
+            if (it == resultados.begin())
+                resp = max(resp, busco);
+            else
+                resp = max(resp, *it + busco);
+        }
+    }
+    cout << resp << endl;
+    return 0;
 }
