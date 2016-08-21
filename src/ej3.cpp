e948aa74318bdc70c8873c474fbd096d9c551df7
Agustin Borgna <hello@aborgna.com.ar>
2016-08-21 13:19:15 -0300

    ej3

--- src/ej3.cpp
+++ src/ej3.cpp
@@ -1,3 +1,28 @@
+#include <stdint.h>
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <algorithm>
 
-int main () {return 0;}
+using namespace std;
+
+int main () {
+    uint64_t n;
+    int64_t res = 0;
+    int64_t border = 0;
+
+    cin >> n;
+
+    for(uint64_t i=0; i<n; i++) {
+        int64_t x;
+        cin >> x;
+
+        border = max((int64_t)0, border+x);
+        res = max(res,border);
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
 
