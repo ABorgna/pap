12378bb6d0c1e1356cd6b0a1533fb98bef0fd11f
Brian Bokser <brian.bokser@gmail.com>
2016-08-21 23:41:32 -0300

    Problema 4: pasa los ejemplos

--- src/ej4.cpp
+++
@@ -1,5 +0,0 @@
-
-#include "matriz.h"
-
-int main () {return 0;}
-
---
+++ src/ejercicio4/ej4.cpp
@@ -0,0 +1,69 @@
+
+#include "matriz.h"
+#include <vector>
+#include <cassert>
+
+bool haySubsecuencia(int start, int end, vector<Matriz>& matrices, Matriz& m, int l){
+  if (end - start < l){
+    return false;
+  }
+  
+  if (end - start == 1){
+    // l == 1
+    return matrices[start] == m;
+  }
+  
+  bool res = false;
+  
+  int mid = (start + end) / 2;
+  
+  vector<Matriz> prefix(l+1);
+  prefix[0] = id();
+  
+  for(int i = 1; i <= l and mid + i - 1 < end; i++){
+    prefix[i] = prefix[i - 1] * matrices[mid + i - 1];
+  }
+
+  if (prefix[l] == m){    res = true;
+    res = true;
+  }
+  
+  Matriz suffix = id();
+  
+  for(int i = 1; i <= l and mid - i >= start; i++){
+    suffix *= matrices[mid - i];
+    if (suffix * prefix[l-i] == m){
+      res = true;
+    }
+  }
+
+  
+  res = res or haySubsecuencia(start, mid, matrices, m, l)
+            or haySubsecuencia(mid, end, matrices, m, l);
+  
+  return res;
+}
+
+int main () {
+  int n, l;
+  std::cin >> n >> l;
+  
+  Matriz m;
+  std::cin>>m;
+  
+  vector<Matriz> matrices(n);
+  
+  for(int i = 0; i < n; i++){
+    std::cin>>matrices[i];
+  }
+
+  bool res = haySubsecuencia(0, n, matrices, m, l);
+  
+  if (res == true){
+    std::cout << "SI\n";
+  }
+  else {
+    std::cout << "NO\n";
+  }
+}
+
