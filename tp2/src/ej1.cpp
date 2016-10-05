fb3557ffc9bc7c8c5a6eaad574b3a1cff237525c
Lucas Bekier <bekierlucas@gmail.com>
2016-10-05 10:01:52 -0300

    corrijo flujo, quedo re re lindo

--- tp2/src/ej1.cpp
+++ tp2/src/ej1.cpp
@@ -5,50 +5,72 @@
 #include <queue>
 using namespace std;
 
-typedef unordered_map<int,int> aristas;
+typedef vector<std::vector<int> > adj_list;
 const int inf = 9999999;
 
-int bfs(vector< aristas > & G,int endpoint){ //Como en la red residual las aristas valen 1 o 0 el camino de aumento siempre puede ser 1 (si es que lo hay) o 0 en caso contrario.
-	queue<int> cola;
-	bool visited [G.size()];
-	fill(visited,visited+endpoint+1,false);
-	cola.push(0);
-	int P[G.size()];
-	fill(P,P + endpoint+1,-1);
-	int aumento = 1;
+int bfs(std::vector<std::vector<int> >& capacidad_red_residual, adj_list& adj, int source, int sink){ 
+//Como en la red residual las aristas valen 1 o 0 el camino de aumento siempre puede ser 1 (si es que lo hay) o 0 en caso contrario.
+	
+	std::queue<int> cola;
+		
+	cola.push(source);
+
+	std::vector<bool> visited(adj.size(), false);
+
+	std::vector<int> p (adj.size(), -1);
+
 	//BFS
+
 	while(!cola.empty()){
 		int actual = cola.front();
 		cola.pop();
+		
 		visited[actual] = true;
-		for(auto &a : G[actual])
-			if(not visited[a.first] and a.second == 1){
-				P[a.first] = actual;
-				cola.push(a.first);
-			}
+		
+		for(auto& v : adj[actual]){
+				if(not visited[v] and capacidad_red_residual[actual][v] == 1){
+					p[v] = actual;
+					cola.push(v);
+				}
+		}
 	} 
+
 	//Si no hubo camino de aumento (no llegue al nodo final)
-	if(not visited[endpoint])
+	if(not visited[sink]){
 		return 0;
-	//En caso de que sí, reconstruyo el camino y a todas esas aristas las marco como usadas y no pueden volver a usarse.
-	int actual = endpoint;
-	int padre = P[actual];
-	while(padre!=-1){
-		G[padre][actual] = 0;
-		G[actual][padre] = 1;
-		actual = padre;
-		padre = P[actual];
 	}
+	
+	//En caso de que sí, reconstruyo el camino y a todas esas aristas las marco como usadas y no pueden volver a usarse.
+	int actual = sink;
+	int padre = p[actual];
+	
+	while(padre!=-1){
+		capacidad_red_residual[padre][actual] = 0;
+		capacidad_red_residual[actual][padre] = 1;
+		
+		actual = padre;
+		padre = p[actual];
+	}
+	// Toda arista tiene capacidad 1
 	return 1;
 }
 
-int flujo(vector< aristas > & G , int endpoint){
+int max_flujo(adj_list& adj, std::vector<std::vector<int>>& capacidad_red_residual, int source, int sink){
 	int flujo_max = 0;
 	int aumento;
-	while((aumento = bfs(G,endpoint))>0){ //Mientras haya camino de aumento
-		flujo_max+=aumento;
-	}
+	
+	do {
+		aumento = bfs(capacidad_red_residual, adj, source, sink);
+		flujo_max += aumento;
+	} while (aumento > 0);
+	
 	return flujo_max;
+}
+
+void conectar(adj_list& G , std::vector<std::vector<int> >&capacidad_red_residual, int a, int b){
+	capacidad_red_residual[a][b] = 1;
+	G[a].push_back(b);
+	G[b].push_back(a);
 }
 
 int main(){
@@ -57,30 +79,36 @@ int main(){
 	int N,M;
 	while(cin>>N>>M){
 		char tipo;
-		vector< aristas > G; //Todo nodo "v" va a ser duplicado por "v_entrantes" y "v_salientes" con una conexión entre ellos direccional. 
+		adj_list G; //Todo nodo "v" va a ser duplicado por "v_entrantes" y "v_salientes" con una conexión entre ellos direccional. 
 		G.resize(2*N+2);
+		
+		std::vector<std::vector<int> > capacidad_red_residual(G.size());
+		for(int i = 0; i < (int) G.size();i++){
+			capacidad_red_residual[i].resize(G.size(),0);
+		}
+
 		int endpoint = 2*N+1;
 		for(int i = 1 ; i < N+1; i ++){
 			cin>>tipo;
 			if(tipo=='E'){
-				G[i][endpoint] = 1; //Las escuelas las conecto con el sumidero
+				conectar(G,capacidad_red_residual,i,endpoint);//Las escuelas las conecto con el sumidero
 			}
 			else{
 				if(tipo=='A'){
-					G[0][i] = 1; //La fuente la conecto con los alumnos.
+					conectar(G,capacidad_red_residual,0,i);//La fuente la conecto con los alumnos.
 				}
 			}
 		}
 		for(int i = 1 ; i < N+1; i ++){
-			G[i][N+i] = 1; //Conecto a todo nodo con su duplicado
+			conectar(G,capacidad_red_residual,i,N+i); //Conecto a todo nodo con su duplicado
 		}
 		for(int i = 0 ; i < M ; i ++){
 			int a,b;
 			cin>>a>>b;	 //Calle bidireccional pero al duplicar los nodos, al saliente lo conecto con el entrante del nodo respectivo.
-			G[N+a][b] = 1;
-			G[b+N][a] = 1;
+			conectar(G, capacidad_red_residual, N+a, b);
+			conectar(G, capacidad_red_residual, b+N, a);
 		}
-		cout<<flujo(G,endpoint)<<endl;
+		cout<<max_flujo(G,capacidad_red_residual,0,endpoint)<<endl;
 	}
 	return 0;
 }
--- tp2/src/ej2.cpp
+++ tp2/src/ej2.cpp
@@ -7,146 +7,137 @@
 #include <vector>
 #include <queue>
 
-using namespace std;
-
 const int inf = 9999999;
 
-typedef vector<vector<int> > adj_list;
+typedef std::vector<std::vector<int> > adj_list;
 
 int nodo_sale(int x, int a){
-  return x;
+	return x;
 }
 
 int nodo_entra(int x, int a){
-  return x + a;
+	return x + a;
 }
 
 
-int bfs(vector<vector<int> >& flujo_neto, adj_list& adj, int source, int sink){ 
-  //Como en la red residual las aristas valen 1 o 0 el camino de aumento siempre puede ser 1 (si es que lo hay) o 0 en caso contrario.
+int bfs(std::vector<std::vector<int> >& capacidad_red_residual, adj_list& adj, int source, int sink){ 
+//Como en la red residual las aristas valen 1 o 0 el camino de aumento siempre puede ser 1 (si es que lo hay) o 0 en caso contrario.
 	
-  queue<int> cola;
-	
-  cola.push(source);
-  
-  vector<bool> visited(adj.size(), false);
+	std::queue<int> cola;
+		
+	cola.push(source);
 
-  vector<int> p (adj.size(), -1);
-  
-  //BFS
-  
+	std::vector<bool> visited(adj.size(), false);
+
+	std::vector<int> p (adj.size(), -1);
+
+	//BFS
+
 	while(!cola.empty()){
 		int actual = cola.front();
 		cola.pop();
 		
-    visited[actual] = true;
+		visited[actual] = true;
 		
-    for(auto& v : adj[actual]){
-			if(not visited[v] and flujo_neto[actual][v] < 1){
-				p[v] = actual;
-				cola.push(v);
-			}
-    }
+		for(auto& v : adj[actual]){
+				if(not visited[v] and capacidad_red_residual[actual][v] == 1){
+					p[v] = actual;
+					cola.push(v);
+				}
+		}
 	} 
-  
+
 	//Si no hubo camino de aumento (no llegue al nodo final)
 	if(not visited[sink]){
 		return 0;
-  }
+	}
 	
-  //En caso de que sí, reconstruyo el camino y a todas esas aristas las marco como usadas y no pueden volver a usarse.
+	//En caso de que sí, reconstruyo el camino y a todas esas aristas las marco como usadas y no pueden volver a usarse.
 	int actual = sink;
 	int padre = p[actual];
 	
-  while(padre!=-1){
-		flujo_neto[padre][actual]++;
-    flujo_neto[actual][padre]--;
-    
-    actual = padre;
+	while(padre!=-1){
+		capacidad_red_residual[padre][actual] = 0;
+		capacidad_red_residual[actual][padre] = 1;
+		
+		actual = padre;
 		padre = p[actual];
 	}
-  
-  // Toda arista tiene capacidad 1
+	// Toda arista tiene capacidad 1
 	return 1;
 }
 
-int max_flujo(adj_list& adj, int source, int sink){
+int max_flujo(adj_list& adj, std::vector<std::vector<int>>& capacidad_red_residual, int source, int sink){
 	int flujo_max = 0;
 	int aumento;
-  
-  vector<vector<int> > flujo_neto(adj.size());
-  for(int i = 0; i < (int) adj.size();i++){
-    flujo_neto[i].resize(adj.size());
-    for(int j = 0; j < (int) adj.size(); j++){
-      flujo_neto[i][j] = 0;
-    }
-  }
-  
-  do {
-    aumento = bfs(flujo_neto, adj, source, sink);
-    flujo_max += aumento;
-    
-  } while (aumento > 0);
-  
-  
+	
+	do {
+		aumento = bfs(capacidad_red_residual, adj, source, sink);
+		flujo_max += aumento;
+	} while (aumento > 0);
+	
 	return flujo_max;
 }
 
 
-bool estaArribaDe(int a, int b, vector<vector<int> >& precios){
-  for(int i = 0; i < (int) precios[a].size(); i++){
-    if (precios[a][i] <= precios[b][i]){ // Ojo, vale el igual?!
-      return false;
-    }
-  }
-  
-  return true;
+bool estaArribaDe(int a, int b, std::vector<std::vector<int> >& precios){
+	for(int i = 0; i < (int) precios[a].size(); i++){
+		if (precios[a][i] <= precios[b][i]){ // Ojo, vale el igual?!
+			return false;
+		}
+	}
+	return true;
 }
 
-int main() {
-  
-  int a, d;
-  
-  std::cin >> a >> d;
-  
-  std::vector<std::vector<int> > precios(a+1);
-  
-  adj_list adj (2*a+ 2); //Todo nodo "v" va a ser duplicado por "v_entrantes" y "v_salientes" con una conexión entre ellos direccional. 
-  
-  int source = 0;
-  int sink = 2*a+1;
+void conectar(adj_list& G , std::vector<std::vector<int> >&capacidad_red_residual, int a, int b){
+	capacidad_red_residual[a][b] = 1;
+	G[a].push_back(b);
+	G[b].push_back(a);
+}
 
-  
-  for (int i = 1; i < a + 1; i++){ // acciones en [1 .. a+1)
-    precios[i].resize(d);
-    
-    for(int j = 0; j < d; j++){
-      std::cin >> precios[i][j];
-    }
-  }
-  
-  for(int i = 1; i < a + 1; i++){
-    adj[nodo_entra(i, a)].push_back(sink);  // con el sink
-    adj[sink].push_back(nodo_entra(i, a));  // con el sink, la vuelta
-    
-    
-    adj[source].push_back(nodo_sale(i, a)); // con el source
-    adj[nodo_sale(i, a)].push_back(source); // con el source
-    
-  }
-  
-  for(int i = 1; i < a + 1; i++){
-    for (int j = 1; j < a + 1; j++){
-      if (estaArribaDe(i, j, precios)){
-    
-        adj[nodo_sale(i, a) ].push_back(nodo_entra(j, a));
-        adj[nodo_entra(j, a)].push_back(nodo_sale(i, a));
-    
-      } 
-    }
-  }
-  
-  int f = max_flujo(adj, source, sink);
-  
-  std::cout << a - f << std::endl;
+
+int main() {
+	
+	int a, d;
+	
+	std::cin >> a >> d;
+	
+	std::vector<std::vector<int> > precios(a+1);
+	
+	adj_list adj (2*a+ 2); //Todo nodo "v" va a ser duplicado por "v_entrantes" y "v_salientes" con una conexión entre ellos direccional. 
+	
+	std::vector<std::vector<int> > capacidad_red_residual(adj.size());
+	for(int i = 0; i < (int) adj.size();i++){
+		capacidad_red_residual[i].resize(adj.size(),0);
+	}
+	
+	int source = 0;
+	int sink = 2*a+1;
+	
+	
+	for (int i = 1; i < a + 1; i++){ // acciones en [1 .. a+1)
+		precios[i].resize(d);
+		
+		for(int j = 0; j < d; j++){
+			std::cin >> precios[i][j];
+		}
+	}
+	
+
+	for(int i = 1; i < a + 1; i++){
+		conectar(adj,capacidad_red_residual,nodo_entra(i, a),sink);
+		conectar(adj,capacidad_red_residual,source, nodo_sale(i,a));
+	}
+	
+	for(int i = 1; i < a + 1; i++){
+		for (int j = 1; j < a + 1; j++){
+			if (estaArribaDe(i, j, precios)){
+				conectar(adj,capacidad_red_residual,nodo_sale(i,a),nodo_entra(j,a));
+			} 
+		}
+	}
+	
+	int f = max_flujo(adj, capacidad_red_residual, source, sink);
+	
+	std::cout << a - f << std::endl;
 }
