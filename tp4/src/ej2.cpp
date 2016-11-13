98d1a0bd8f5feaf1f0056e9d1cb80e9f2f2ad16d
Agustin Borgna <hello@aborgna.com.ar>
2016-11-13 18:13:03 -0300

    ej2

--- tp4/src/ej2.cpp
+++ tp4/src/ej2.cpp
@@ -1,9 +1,36 @@
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
-#include <map>
 #include <vector>
 
 using namespace std;
 
-int main(){
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    double res = 1;
+    auto it = v.begin();
+
+    while (it != v.end()) {
+        auto itNext =
+            find_if_not(it + 1, v.end(), [&it](int k) { return k == *it; });
+
+        int count = distance(it, itNext);
+        int restantes = distance(it, v.end());
+        for (int i = 0; i < count; i++) {
+            res += (double)(restantes - i) / (double)(count - i) - 1;
+        }
+
+        it = itNext;
+    }
+
+    cout << fixed << setprecision(6) << res << endl;
+
     return 0;
 }
